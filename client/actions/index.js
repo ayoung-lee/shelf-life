891454fab2e2bf801e86e37e3a94f49e8d594ba1
ayoung lee <aylee0729@gmail.com>
2022-06-28 18:08:45 +1200

    delete from the array

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,8 +1,16 @@
 export const ADD_INGREDIENT = 'ADD_INGREDIENT'
+export const DEL_INGREDIENT = 'DEL_INGREDIENT'
 
 export const addIngredient = (ingredient) => {
   return {
     type: ADD_INGREDIENT,
+    payload: ingredient,
+  }
+}
+
+export const delIngredient = (ingredient) => {
+  return {
+    type: DEL_INGREDIENT,
     payload: ingredient,
   }
 }
